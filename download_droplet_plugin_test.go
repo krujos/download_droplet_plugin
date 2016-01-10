dae8457bbf6a9e6a6c3bcc37171fe99d1cb02915
krujos <jkruck@pivotal.io>
2016-01-09 19:00:00 -0600

    Add ginkgo

---
+++ download_droplet_plugin_test.go
@@ -0,0 +1,12 @@
+package main_test
+
+import (
+	. "github.com/krujos/download_droplet_plugin"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("DownloadDropletPlugin", func() {
+
+})

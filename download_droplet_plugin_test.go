2e0990cc80c0a35ae7489f81ab07f3392a41d5e8
krujos <jkruck@pivotal.io>
2016-01-11 00:23:15 -0600

    refactor packages

--- download_droplet_plugin_test.go
+++ download_droplet_plugin_test.go
@@ -1,15 +1,11 @@
 package main_test
 
 import (
-	"errors"
 	"os/exec"
 
-	cli_fakes "github.com/cloudfoundry/cli/plugin/fakes"
-	io_helpers "github.com/cloudfoundry/cli/testhelpers/io"
 	"github.com/cloudfoundry/cli/testhelpers/rpc_server"
 	fake_rpc_handlers "github.com/cloudfoundry/cli/testhelpers/rpc_server/fakes"
 	. "github.com/krujos/download_droplet_plugin"
-	fake_droplet "github.com/krujos/download_droplet_plugin/droplet/fakes"
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
@@ -20,78 +16,6 @@ import (
 const pluginPath = "./download-droplet-plugin"
 
 var _ = Describe("DownloadDropletPlugin", func() {
-
-	var (
-		fakeCliConnection     *cli_fakes.FakeCliConnection
-		downloadDropletPlugin *DownloadDropletCmd
-		goodArgs              []string
-		uninstallArgs         []string
-		fakeDroplet           *fake_droplet.FakeDroplet
-	)
-
-	BeforeEach(func() {
-		fakeCliConnection = &cli_fakes.FakeCliConnection{}
-		fakeDroplet = new(fake_droplet.FakeDroplet)
-		downloadDropletPlugin = &DownloadDropletCmd{}
-		downloadDropletPlugin.Drop = fakeDroplet
-		goodArgs = []string{"download-droplet", "theApp", "/tmp"}
-		uninstallArgs = []string{"CLI-MESSAGE-UNINSTALL"}
-	})
-
-	Describe("GetMetadata", func() {
-
-		It("Returns metadata", func() {
-			md := downloadDropletPlugin.GetMetadata()
-			Ω(md).NotTo(BeNil())
-		})
-
-		It("Has a help message", func() {
-			md := downloadDropletPlugin.GetMetadata()
-			Ω(md.Commands[0].HelpText).NotTo(BeNil())
-		})
-	})
-
-	Describe("Run", func() {
-		Context("Messages", func() {
-			It("pritns an informative message when downloading the droplet", func() {
-				output := io_helpers.CaptureOutput(func() {
-					downloadDropletPlugin.Run(fakeCliConnection, goodArgs)
-				})
-				Ω(output[0]).To(Equal("Saving theApp's droplet to /tmp"))
-			})
-		})
-
-		Context("initializer complication", func() {
-			It("Should call the initializer during run", func() {
-				Fail("NYI")
-			})
-		})
-
-		Context("Saving a droplet", func() {
-			It("should call save dropplet with the right arguments", func() {
-				downloadDropletPlugin.Run(fakeCliConnection, goodArgs)
-				name, path := fakeDroplet.SaveDropletArgsForCall(0)
-				Ω(name).Should(Equal("theApp"))
-				Ω(path).Should(Equal("/tmp"))
-				Ω(fakeDroplet.SaveDropletCallCount()).To(Equal(1))
-			})
-
-			It("Should print an error message if an err is returned", func() {
-				fakeDroplet.SaveDropletReturns(errors.New("This is an error"))
-				output := io_helpers.CaptureOutput(func() {
-					downloadDropletPlugin.Run(fakeCliConnection, goodArgs)
-				})
-				Ω(output[1]).To(Equal("This is an error"))
-			})
-
-			It("Should print an uninstall message", func() {
-				output := io_helpers.CaptureOutput(func() {
-					downloadDropletPlugin.Run(fakeCliConnection, uninstallArgs)
-				})
-				Ω(output[0]).To(Equal("Thanks for using droplet downloader!"))
-			})
-		})
-	})
 
 	Describe("Run - integration tests", func() {
 		var (
--- init/plugin_initializer.go
+++
@@ -1,8 +0,0 @@
-package init
-
-import "github.com/cloudfoundry/cli/plugin"
-
-//PluginInitializer provides IOC for plugin initialization
-type PluginInitializer interface {
-	InitializePlugin(cmd *DownloadDropletCmd, cli plugin.CliConnection) error
-}
